dad42a41caace799bc7c22165de55e96ec15f6ea
lesidikariuki-star <>
2026-07-20 12:45:56 +0000

    String manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,13 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+
+# Extract dairy and bakery items using slicing
+dairy1 = 'milk' [0:4]    # 'milk'
+dairy2 = 'cheese' [5:11]   # 'cheese'
+bakery1 = 'bread' [12:17] # 'bread'
+
+
+print("We have dairy and bakery items: " 
+      + dairy1 + ", " 
+      + dairy2 + ", and " 
+      + bakery1 
+      + " in aisle 5")
